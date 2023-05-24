2f073cd05d8affeac7a6a6cf476742177d63d555
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-05-24 01:51:42 +0000

    [検索機能の実装]

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
-    
+  get "search" => "searches#search"
   get 'home/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
