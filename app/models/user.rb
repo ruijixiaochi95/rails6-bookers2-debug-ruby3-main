db2f67a3451341d34ebe6fe63388942844ab169d
ruijixiaochi95 <132245275+ruijixiaochi95@users.noreply.github.com>
2023-06-03 09:58:12 +0000

    [DM機能の実装　途中]

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,32 +8,35 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
-  
+  has_many :user_rooms
+  has_many :chats
+  has_many :rooms, through: :user_rooms
+
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
   has_many :followings, through: :relationships, source: :followed
-  
+
   has_many :reverse_of_relationships,class_name:"Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: {maximum: 50 }, uniqueness: false
-  
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
-  
+
   def follow(user_id)
     relationships.create(followed_id: user_id)
-  end 
-  
+  end
+
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
-  end 
-  
+  end
+
   def following?(user)
     followings.include?(user)
   end
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @user = User.where("name LIKE?", "#{word}")
@@ -43,9 +46,9 @@ class User < ApplicationRecord
       @user = User.where("name LIKE?", "%#{word}")
     elsif search == "partial_match"
       @user = User.where("name LIKE?", "%#{word}%")
-    else 
+    else
       @user = User.all
-    end 
-  end 
-  
+    end
+  end
+
 end
